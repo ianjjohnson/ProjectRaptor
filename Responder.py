0a1883a885bf09a4f78e69d50699d1338fb1f8f5
Ian Johnson <ianjjohnson@icloud.com>
2015-12-23 14:51:11 -0700

    Add basic responder, mostly a port of Text.py

---
+++ Responder.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import smptlib
+import time
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+
+def buildMessage(sender, recipient, subject, body):
+	return """\
+	From: %s
+	To: %s
+	Subject: %s
+	\n
+	%s
+	""" % (sender, recipient, subject, body)
+
+def sendMessage(body):
+	body = body + ""
+		
+	message = buildMessage("ianlinuxserver@gmail.com", "3038153710@mms.att.net", "New System!", "This is a test message from the new texting system")
+
+	mailServer = smtplib.SMTP("smtp.gmail.com:587")
+	mailServer.starttls()
+	mailServer.login("ianlinuxserver@gmail.com", "LinuxMint2015!")
+	mailServer.sendMail("ianlinuxserver@gmail.com", "3038153710@mms.att.net", body)
+
+	mailServer.quit()
+	print("Sent Text:" + body)
+
+sendMessage("Swag on Rye!")
+	
